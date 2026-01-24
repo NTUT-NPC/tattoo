ec7261e46418928a7566393f29e5788c8393286f
Dao_you <qaz00639@gmail.com>
2026-01-25 04:59:01 +0800

    fix: prevent welcome intro overflow on small screens
    - allow scrolling in welcome intro
    - keep swipe indicator pinned

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -2,15 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:tattoo/welcome_introduction_page.dart';
 import 'package:tattoo/welcome_login_page.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: _pageController,
         scrollDirection: Axis.vertical,
-        children: const [WelcomeIntroductionPage(), WelcomeLoginPage()],
+        children: [
+          WelcomeIntroductionPage(
+            onRequestNextPage: () {
+              if (!_pageController.hasClients) return;
+              // Called by the intro page when the user swipes up at the bottom.
+              _pageController.nextPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeOut,
+              );
+            },
+          ),
+          const WelcomeLoginPage(),
+        ],
       ),
     );
   }
