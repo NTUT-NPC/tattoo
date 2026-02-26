b070840d767cda4d3a753acbb534202d69b6244b
Riley Ho <riley@rileychh.dev>
2026-02-27 05:08:10 +0800

    feat: add typed preference keys with get/set access
    Add PrefKey<T> enum for type-safe preference access with defaults,
    and implement get()/set() methods on PreferencesRepository.
    
    [skip upload]

--- lib/repositories/preferences_repository.dart
+++ lib/repositories/preferences_repository.dart
@@ -1,8 +1,20 @@
-// ignore_for_file: unused_field
-
 import 'package:riverpod/riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tattoo/utils/shared_preferences.dart';
+
+/// SharedPreferences value type, used to dispatch to the correct accessor.
+enum PrefType { boolean, integer, double, string, stringList }
+
+// dart format off
+/// Typed preference keys with defaults.
+enum PrefKey<T> {
+  demoMode<bool>(PrefType.boolean, false);
+
+  const PrefKey(this.type, this.defaultValue);
+  final PrefType type;
+  final T defaultValue;
+}
+// dart format on
 
 /// Provides the [PreferencesRepository] instance.
 final preferencesRepositoryProvider = Provider<PreferencesRepository>((ref) {
@@ -18,4 +30,32 @@ class PreferencesRepository {
   PreferencesRepository({
     required SharedPreferencesAsync prefs,
   }) : _prefs = prefs;
+
+  /// Gets a preference value, returning the key's default if not set.
+  Future<T> get<T>(PrefKey<T> key) async {
+    final value = switch (key.type) {
+      PrefType.boolean => await _prefs.getBool(key.name),
+      PrefType.integer => await _prefs.getInt(key.name),
+      PrefType.double => await _prefs.getDouble(key.name),
+      PrefType.string => await _prefs.getString(key.name),
+      PrefType.stringList => await _prefs.getStringList(key.name),
+    };
+    return (value as T?) ?? key.defaultValue;
+  }
+
+  /// Sets a preference value.
+  Future<void> set<T>(PrefKey<T> key, T value) async {
+    switch (key.type) {
+      case PrefType.boolean:
+        await _prefs.setBool(key.name, value as bool);
+      case PrefType.integer:
+        await _prefs.setInt(key.name, value as int);
+      case PrefType.double:
+        await _prefs.setDouble(key.name, value as double);
+      case PrefType.string:
+        await _prefs.setString(key.name, value as String);
+      case PrefType.stringList:
+        await _prefs.setStringList(key.name, value as List<String>);
+    }
+  }
 }
