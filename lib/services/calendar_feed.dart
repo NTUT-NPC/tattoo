95f3bc25025a87b621238cec024d6046db0781b2
Kevin <cgshyuankai@proton.me>
2026-02-27 17:06:22 +0800

    feat: refactor calendar URL generation with utility functions for ICS and embed links

--- lib/services/calendar_feed.dart
+++ lib/services/calendar_feed.dart
@@ -1,2 +1,19 @@
-const calendarPublicIcsUrl =
-    'https://calendar.google.com/calendar/ical/docfuhim9b22fqvp2tk842ak3c%40group.calendar.google.com/public/basic.ics';
+const calendarId = 'docfuhim9b22fqvp2tk842ak3c@group.calendar.google.com';
+const calendarTimeZone = 'Asia/Taipei';
+
+Uri buildCalendarPublicIcsUri(String id) {
+    return Uri.https(
+        'calendar.google.com',
+        '/calendar/ical/$id/public/basic.ics',
+    );
+}
+
+Uri buildCalendarEmbedUri(String id, {String timeZone = calendarTimeZone}) {
+    return Uri.https('calendar.google.com', '/calendar/embed', {
+        'src': id,
+        'ctz': timeZone,
+    });
+}
+
+String get calendarPublicIcsUrl => buildCalendarPublicIcsUri(calendarId).toString();
+String get calendarEmbedUrl => buildCalendarEmbedUri(calendarId).toString();
