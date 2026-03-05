5bbf5f69ed17a4bf1a197ba11a683518522a82b5
Dao-you <qaz00639@gmail.com>
2026-03-05 09:47:51 +0800

    feat: add hasNCourse in CourseTableSummaryObject

--- lib/repositories/course_repository.dart
+++ lib/repositories/course_repository.dart
@@ -72,6 +72,7 @@ typedef CourseTableSummaryObject = ({
 
   /// Whether the table has courses in the morning (before 12:00).
   bool hasAmCourse,
+  bool hasNCourse,
   bool hasPmCourse,
   bool hasNightCourse,
   Period earliestStartSection,
