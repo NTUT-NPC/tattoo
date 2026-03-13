d3d7ba864c91f154f64b1044cb6bb36ca345bbc6
Riley Ho <riley@rileychh.dev>
2026-03-13 22:25:20 +0800

    fix: revert unnecessary schemaVersion bump

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -57,7 +57,7 @@ class AppDatabase extends _$AppDatabase {
   AppDatabase([QueryExecutor? executor]) : super(executor ?? _openConnection());
 
   @override
-  int get schemaVersion => 2;
+  int get schemaVersion => 1;
 
   @override
   MigrationStrategy get migration => MigrationStrategy(
