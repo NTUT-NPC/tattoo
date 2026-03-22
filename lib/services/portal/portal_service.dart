df8f4b719b47a1a791353070d0544b8b79f91997
Dao-you <qaz00639@gmail.com>
2026-03-23 04:21:22 +0800

    feat: add some systems

--- lib/services/portal/portal_service.dart
+++ lib/services/portal/portal_service.dart
@@ -82,7 +82,10 @@ enum PortalServiceCode {
   networkAndInfoSecurityManagementSystem('ipmac_oauth'),
   campusLicensedSoftware('inf001_oauth'),
   webmail('zimbrasso_oauth'),
-  ntutPostman('test_postman');
+  ntutPostman('test_postman'),
+  librarySystem('lib_002_oauth'),
+  studentLoanSystem('sa_SLAS_oauth'); 
+
   // TODO: --- for portal screen demo, delete after migration ---
 
 
